8fcc4a9cba1bc1ddf082b11194fa620ae596d009
TabbyGreen <159805746+TabbyGreen@users.noreply.github.com>
2024-02-16 11:56:03 -0500

    Add files via upload
    Submission 1

---
+++ GreenTabatha.py
@@ -0,0 +1,20 @@
+#Tabatha Green
+#2/12/2024
+#P1Lab1
+#Intro to Python- practice
+
+#create a variable
+fave_candy = "Almond joy"
+
+#print our fave candy
+print ("my favorite candy is", fave_candy)
+
+#Get some information (input) from the user
+fave_candy = input ("Enter your favorite candy: ")
+
+#Use intergers
+num_candy = 15
+eaten_candy = 7
+
+candy_left = num_candy - eaten_candy
+print ("How much candy is left", candy_left)
